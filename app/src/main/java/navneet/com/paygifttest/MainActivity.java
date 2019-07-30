2477c68086d4b0625976f06b0883695c9d95b447
Navneet Krishna <krisnavneet@gmail.com>
2019-07-30 18:57:29 +0530

    created method getNewsFeed() for network call and successfully fetched json data

--- app/src/main/java/navneet/com/paygifttest/MainActivity.java
+++ app/src/main/java/navneet/com/paygifttest/MainActivity.java
@@ -3,10 +3,17 @@ package navneet.com.paygifttest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import navneet.com.paygifttest.model.Article;
+import navneet.com.paygifttest.model.NewsResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getNewsFeed(); //fetches the json response from newsapi.org using retrofit
+    }
+
+    private void getNewsFeed() {
+
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://newsapi.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();  //creating a retrofit object and loading it with gson converter factory for json to pojo conversions easy
+
+        RequestInterface requestInterface=retrofit.create(RequestInterface.class); // request interface is where we define all our endpoints
+        Call<NewsResponse> call=requestInterface.getNewsFeed();
+
+        call.enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                Toast.makeText(MainActivity.this,"Success!",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this,"Failure!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/navneet/com/paygifttest/RequestInterface.java
@@ -0,0 +1,13 @@
+package navneet.com.paygifttest;
+
+import navneet.com.paygifttest.model.NewsResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Navneet Krishna on 30/07/19.
+ */
+interface RequestInterface {
+    @GET("v2/top-headlines?country=us&apiKey=9bce785607fc41c3b24cb48efe043d2f")
+    Call<NewsResponse> getNewsFeed();
+}
